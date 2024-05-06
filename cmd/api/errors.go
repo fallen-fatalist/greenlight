23099788680e13c2e57265809875847817d77800
Murat <m_kairbek@outlook.com>
2024-05-07 00:14:06 +0500

    added badRequestHandler;

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -34,5 +34,8 @@ func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request)
 func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
 
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
