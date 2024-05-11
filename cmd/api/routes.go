2f8c84196cd8a351497c1977834664dad1dfee14
Murat <m_kairbek@outlook.com>
2024-05-12 01:26:08 +0500

    added route in router for updating movies;

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,10 +13,11 @@ func (app *application) routes() *httprouter.Router {
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
-	// router api handlers
+	// router api handlers (order of router handlers the same as the order of declarations of the handlers of movies)
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMovieHandler)
 
 	return router
 }
