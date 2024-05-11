880219bb4451c128712d61e795e4996c1590ef0d
Murat <m_kairbek@outlook.com>
2024-05-12 02:35:20 +0500

    changed updateMovieHandler from replacing updates to partial updates;

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -99,9 +99,9 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	var input struct {
-		Title   string       `json: "title"`
-		Year    int32        `json: "year"`
-		Runtime data.Runtime `json: "runtime"`
+		Title   *string       `json: "title"`
+		Year    *int32        `json: "year"`
+		Runtime *data.Runtime `json: "runtime"`
 		Genres  []string     `json: "genres"`
 	}
 
@@ -110,11 +110,20 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.badRequestResponse(w, r, err)
 		return
 	}
+	
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
 
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
 
 	v := validator.New()
 
