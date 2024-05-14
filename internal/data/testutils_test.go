377f7dd5ccb4bbc7bded8963439cca5615fa7e37
Murat <m_kairbek@outlook.com>
2024-05-14 09:56:13 +0500

    added utility which returns test database;

---
+++ internal/data/testutils_test.go
@@ -0,0 +1,42 @@
+package data
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+)
+
+func newTestDB(t *testing.T) *sql.DB {
+	db, err := sql.Open("postgresql", "postgres://test_web:@localhost/greenlight_test?sslmode=disable")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	script, err := os.ReadFile("./testdata/setup.sql")
+	if err != nil {
+		db.Close()
+		t.Fatal(err)
+	}
+
+	_, err = db.Exec(string(script))
+	if err != nil {
+		db.Close()
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		defer db.Close()
+
+		script, err := os.ReadFile("./testdata/teardown.sql")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = db.Exec(string(script))
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	return db
+}
